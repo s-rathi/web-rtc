1b231c535741ded0090ed5043d2a5ec8e23dea48
Shikha Rathi <65958533+s-rathi@users.noreply.github.com>
2021-10-09 22:44:40 +0900

    Handled Socket Backend (#4)

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,82 @@
 import express, { Application } from "express";
 import { Server as SocketIOServer } from "socket.io";
 import { createServer, Server as HTTPServer } from "http";
+import path from "path";
 
 export class Server {
   private httpServer: HTTPServer;
   private app: Application;
   private io: SocketIOServer;
+  private activeSockets: string[];
 
   private readonly DEFAULT_PORT = 5000;
 
   constructor() {
-      this.app = express();
-      this.httpServer = createServer(this.app);
-      this.io = new SocketIOServer(this.httpServer);
+    this.app = express();
+    this.httpServer = createServer(this.app);
+    this.io = new SocketIOServer(this.httpServer);
+    this.activeSockets = [];
 
-      this.handleRoutes();
-      this.handleSocketConnection();
+    this.configureApp();
+    this.handleSocketConnection();
   }
 
-  private handleRoutes(): void {
-      this.app.get("/", (req, res) => {
-        res.send(`<h1>WebRTC App</h1>`); 
-      });
+  private configureApp(): void {
+    this.app.use(express.static(path.join(__dirname, "../public")));
   }
 
   private handleSocketConnection(): void {
     this.io.on("connection", socket => {
-      console.log("Socket connected.");
+      const existingSocket = this.activeSockets.find(
+        existingSocket => existingSocket === socket.id
+      );
+
+      if (!existingSocket) {
+        this.activeSockets.push(socket.id);
+
+        socket.emit("update-user-list", {
+          users: this.activeSockets.filter(
+            existingSocket => existingSocket !== socket.id
+          )
+        });
+
+        socket.broadcast.emit("update-user-list", {
+          users: [socket.id]
+        });
+      }
+
+      socket.on("call-user", (data: any) => {
+        socket.to(data.to).emit("call-made", {
+          offer: data.offer,
+          socket: socket.id
+        });
+      });
+
+      socket.on("make-answer", data => {
+        socket.to(data.to).emit("answer-made", {
+          socket: socket.id,
+          answer: data.answer
+        });
+      });
+
+      socket.on("reject-call", data => {
+        socket.to(data.from).emit("call-rejected", {
+          socket: socket.id
+        });
+      });
+
+      socket.broadcast.emit("update-user-list", {
+        users: [socket.id]
+      });
+
+      socket.on("disconnect", () => {
+        this.activeSockets = this.activeSockets.filter(
+          existingSocket => existingSocket !== socket.id
+        );
+        socket.broadcast.emit("remove-user", {
+          socketId: socket.id
+        });
+      });
     });
   }
 
